e9a87de61972dd31362ae2a70b7bd9d5b67131b9
Panu Valtanen <p4nu@users.noreply.github.com>
2021-06-13 11:38:40 +0300

    Edit LearningSpringApplication Add temporary class RoomController to test query functionality for Room entity and its repository

--- src/main/java/com/panuvaltanen/lil/learningspring/LearningSpringApplication.java
+++ src/main/java/com/panuvaltanen/lil/learningspring/LearningSpringApplication.java
@@ -1,7 +1,13 @@
 package com.panuvaltanen.lil.learningspring;
 
+import com.panuvaltanen.lil.learningspring.data.entity.Room;
+import com.panuvaltanen.lil.learningspring.data.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class LearningSpringApplication {
@@ -10,4 +16,16 @@ public class LearningSpringApplication {
         SpringApplication.run(LearningSpringApplication.class, args);
     }
 
+    // Temporarily added this here to test functionality
+    @RestController
+    @RequestMapping("/rooms")
+    public class RoomController {
+        @Autowired
+        private RoomRepository roomRepository;
+
+        @GetMapping
+        public Iterable<Room> getRooms() {
+            return this.roomRepository.findAll();
+        }
+    }
 }
