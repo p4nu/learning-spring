c7881d9cdecb433adf412dbfc4c8c80c0fd1cec5
Panu Valtanen <p4nu@users.noreply.github.com>
2021-06-16 12:49:47 +0300

    Edit LearningSpringApplication.java Add temporary Controller classes to test functionality

--- src/main/java/com/panuvaltanen/lil/learningspring/LearningSpringApplication.java
+++ src/main/java/com/panuvaltanen/lil/learningspring/LearningSpringApplication.java
@@ -1,6 +1,10 @@
 package com.panuvaltanen.lil.learningspring;
 
+import com.panuvaltanen.lil.learningspring.data.entity.Guest;
+import com.panuvaltanen.lil.learningspring.data.entity.Reservation;
 import com.panuvaltanen.lil.learningspring.data.entity.Room;
+import com.panuvaltanen.lil.learningspring.data.repository.GuestRepository;
+import com.panuvaltanen.lil.learningspring.data.repository.ReservationRepository;
 import com.panuvaltanen.lil.learningspring.data.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -16,7 +20,7 @@ public class LearningSpringApplication {
         SpringApplication.run(LearningSpringApplication.class, args);
     }
 
-    // Temporarily added this here to test functionality
+    // TODO: Put this in a new controller file
     @RestController
     @RequestMapping("/rooms")
     public class RoomController {
@@ -26,6 +30,32 @@ public class LearningSpringApplication {
         @GetMapping
         public Iterable<Room> getRooms() {
             return this.roomRepository.findAll();
+        }
+    }
+
+    // TODO: Put this in a new controller file
+    @RestController
+    @RequestMapping("/guests")
+    public class GuestController {
+        @Autowired
+        private GuestRepository guestRepository;
+
+        @GetMapping
+        public Iterable<Guest> getGuests() {
+            return this.guestRepository.findAll();
+        }
+    }
+
+    // TODO: Put this in a new controller file
+    @RestController
+    @RequestMapping("/reservations")
+    public class ReservationController {
+        @Autowired
+        private ReservationRepository reservationRepository;
+
+        @GetMapping
+        public Iterable<Reservation> getReservations() {
+            return this.reservationRepository.findAll();
         }
     }
 }
